10462efaaf01d98c389ac6571db77b964fd49b7f
BrunosCode <BrunosCode@users.noreply.github.com>
2024-11-24 16:33:40 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,7 +26,7 @@ class TestCase extends Orchestra
         Artisan::call('migrate');
     }
 
-    protected function testConfig(array $config = [])
+    protected function test_config(array $config = [])
     {
         return array_merge([
             'keyDelimiter' => '.',
